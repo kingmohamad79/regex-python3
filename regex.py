6ded435401ff0990d429d106c52223a219ab2eef
mohammad nouri <norimohamad115@gmail.com>
2024-04-04 14:48:07 +0330

    final commit

--- regex.py
+++ regex.py
@@ -5,3 +5,5 @@ with open(file=r'C:\Users\User\Desktop\regex in python\text.txt', mode='r+') as 
     z = read.read()
     x = re.sub(pattern='\s*d[a-z]{3}r\s*',repl=' king ',string=z)
     read.write(x)
+
+print('x')
